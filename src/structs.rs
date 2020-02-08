8c04f8b8e3747a227b121f88590a9ddd6ee1d607
Arjun Biyani <Arjunbiyani@ACKO522.local>
2020-02-08 18:16:20 +0530

    structs and functions

--- src/structs.rs
+++ src/structs.rs
@@ -20,6 +20,9 @@ impl Product {
     fn render_fullname(&self) -> String{
         format!("{} {}",self.p_name,self.p_name)
     }
+    fn change_product_name(&mut self, pname: &str) {
+        self.p_name=pname.to_string(); 
+    }
 }
 
 //creating function 
@@ -29,9 +32,9 @@ impl Product {
 
 pub fn run(){
     let mut p = Product::new(1,"Macbook",222);
-
+   p.change_product_name("Macbook New");
     let mut l = location(1,String::from("Bangalore"),3);
-    //p.p_name=String::from("Macbook");
+    //p.p_name=String::from("Macbook ");
     println!("{} ",p.render_fullname());
     println!("location {} - {} - {} ",l.0,l.1,l.2 );
 }
